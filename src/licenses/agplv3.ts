971eb6f588f82f8ec7806ee0dc35f08907662b59
Teddy Rilliot <trilliot@users.noreply.github.com>
2020-05-30 16:37:54 +0200

    add header, previously removed by mistake

--- src/licenses/agplv3.ts
+++ src/licenses/agplv3.ts
@@ -691,4 +691,22 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 <http://www.gnu.org/licenses/>.`
         return template;
     }
+
+    public header(): string {
+        let template = `Copyright (C) ${this.year} ${this.author}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.`
+        return template;
+    }
 }

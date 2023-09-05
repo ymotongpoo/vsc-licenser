02ea124d36eee31508a97028057674811a125305
Julia DeMille <me@jdemille.com>
2023-09-05 11:53:28 -0500

    Fix link in header to specify EUPL-1.2 instead of just EUPL. Should always point to version 1.2.

--- src/licenses/eupl12.ts
+++ src/licenses/eupl12.ts
@@ -320,7 +320,7 @@ Licensed under the EUPL, Version 1.2
 
 You may not use this work except in compliance with the Licence.
 You should have received a copy of the Licence along with this work. If not, see:
-<https://joinup.ec.europa.eu/software/page/eupl>.
+<https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
 See the Licence for the specific language governing permissions and limitations under the Licence.`
         return template;
     }
